0588379e90de771d967b3968b82a434928447714
cyerinn <cyr02201@gmail.com>
2025-05-15 15:28:30 +0900

    fix: new build

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
 from setuptools import setup
 from setuptools.dist import Distribution
-import os
 
 class BinaryDistribution(Distribution):
-    def has_ext_modules(foo):
+    def has_ext_modules(self):
         return True
 
 setup(
-    name='opencv-python-gstreamer',
+    name='opencv_python_gstreamer',
     version='4.12.0.dev0',
+    description='Custom OpenCV build with GStreamer for Jetson',
     packages=['cv2'],
     package_dir={'cv2': 'cv2'},
-    package_data={'cv2': ['*.so']},
-    include_package_data=True,
-    zip_safe=False,
+    package_data={'cv2': ['cv2.cpython-310-aarch64-linux-gnu.so']},
     distclass=BinaryDistribution,
+    zip_safe=False,
 )
