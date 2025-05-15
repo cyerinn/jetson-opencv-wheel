7209c1b260d69c0dec16aa650fa72b373e8236f6
cyerinn <cyr02201@gmail.com>
2025-05-15 11:04:51 +0900

    fix: 빌드에 .so 파일 누락 문제 해결

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 from setuptools import setup
-from setuptools.extension import Extension
+from setuptools.dist import Distribution
+import os
+
+class BinaryDistribution(Distribution):
+    def has_ext_modules(foo):
+        return True
 
 setup(
     name='opencv-python-gstreamer',
     version='4.12.0.dev0',
-    description='OpenCV with GStreamer for Jetson',
     packages=['cv2'],
     package_dir={'cv2': 'cv2'},
+    package_data={'cv2': ['*.so']},
+    include_package_data=True,
+    zip_safe=False,
+    distclass=BinaryDistribution,
 )
-
